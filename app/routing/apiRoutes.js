3b07c02ae7f465e50f2488a39add7a63fc103f56
Charles <regalferg@gmail.com>
2018-01-19 20:29:25 -0500

    Comments for Code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 
 module.exports = function(app){
 
- //a GET route that displays JSON of all possible pagans
+ //GET route that displays current pagans
   app.get('/api/pagans', function(req,res){
     res.json(pagans);
   });
@@ -14,36 +14,39 @@ module.exports = function(app){
     var newPagan = req.body;
     console.log(newPagan);
     console.log(pagans);
-    //grabs the new friend's scores to compare with pagans in pagans array
+    //Gets the new pagan's scores to compare with pagans in pagans array
     var newPaganScores = newPagan.scores;
     var scoresArr = [];
     var paganCount = 0;
     var bestMatch = 0;
 console.log(newPaganScores);
-    //runs through all current pagans in list
+
+    //Loops through all current pagans 
     for(var i=0; i < pagans.length; i++){
       var scoresDiff = 0;
-      //run through scores to compare pagans
+
+
+      //Loops through scores to compare pagans
       for(var j=0; j < newPaganScores.length; j++){
         scoresDiff += (Math.abs(parseInt(pagans[i].scores[j]) - parseInt(newPaganScores[j])));
       }
 
-      //push results into scoresArr
+      //Push results into scoresArr
       scoresArr.push(scoresDiff);
     }
 
-    //After all pagans are compared, find best match
+    //After all pagans are compared, finds match
     for(var i=0; i < scoresArr.length; i++){
       if(scoresArr[i] <= scoresArr[bestMatch]){
         bestMatch = i;
       }
     }
 
-    // Return bestMatch data
+    // Returns bestMatch data
     var paganMatch = pagans[bestMatch];
     res.json(paganMatch);
 
-    //pushes new submission into the pagansList array
+    //Push new submission into the pagans array
     pagans.push(newPagan);
     // res.json(newPagan);
   });
